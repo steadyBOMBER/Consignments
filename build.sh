04ca070f719dc3f0ba9b565e5a5a11c49688b689
steadyBOMBER <169099503+steadyBOMBER@users.noreply.github.com>
2025-10-05 03:14:30 -0700

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+pip install -r requirements.txt
+if ! command -v rustc &> /dev/null; then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    source $HOME/.cargo/env
+fi
