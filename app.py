0fd4ab419e17ef2b0f04c8e5700cb528a6c3813a
steadyBOMBER <169099503+steadyBOMBER@users.noreply.github.com>
2025-10-06 15:32:47 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-```python
 import os
 import logging
 import smtplib
@@ -1353,4 +1352,3 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     socketio.run(app, debug=False, host='0.0.0.0', port=5000)
-```
